3c96591bbb6b0766c941fd77274d96ad18d2e45a
khoirunnisaaaa <k.nisa11602@gmail.com>
2021-11-19 08:26:49 +0700

    update alamat css
    update alamat css

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ if(isset($_POST['submit'])){
 <html>
 <head>
     <title>Login Page </title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/login-register/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
