95d084a4298131ef1668fb66539d78844b4b7e7d
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2021-12-02 18:29:34 +0700

    Update delete_task.php

--- delete_task.php
+++ delete_task.php
@@ -1,8 +1,7 @@
 <?php
 require ('koneksi.php');
 $id =$_GET['id_task'];
-$hari = $_GET['hari'];
 $query = "DELETE FROM college_task WHERE id_task='$id'";
 $result = mysqli_query($koneksi, $query)or die (mysql_error());;
-header('Location: task.php);
+header('Location: task.php');
 ?>
