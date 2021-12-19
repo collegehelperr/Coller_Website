d93052319d738495ddbf454c36c064524700375c
Khoirunnisa <k.nisa11602@gmail.com>
2021-12-19 15:01:18 +0700

    progress-bar revision

--- dashboard.php
+++ dashboard.php
@@ -260,7 +260,6 @@ $connection = require_once 'db_conn.php';
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
     <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="Dynamic-Circular-Progress-Bar-with-jQuery-CSS3/jQuery-plugin-progressbar.js"></script>
     <script>
