e6d66cf299ac305cf70cd2ec503866cddbaa5fb1
NabiylaHamestuti14 <naufalhisyam266@gmail.com>
2021-12-25 16:36:23 +0700

    Update dashboard.php
    update dashboard

--- dashboard.php
+++ dashboard.php
@@ -316,6 +316,26 @@ $connection = require_once 'db_conn.php';
                 );
             });
         });
+
+        $(document).ready(function(){
+                $(".form-check-input").click(function(e) {
+                    const id = $(this).attr("data-task-id");
+                    
+                    $.post('app/task_status.php', {
+                            id:id
+                        }, 
+                        (data) => {
+                            if (data != 'error') {
+                            const h2 = $(this).next();
+                            if (data === '1') {
+                                h2.removeClass('checked');
+                            } else {
+                                h2.addClass('checked');
+                            }
+                        }
+                        })
+                });
+            });
     </script>
 </body>
 
